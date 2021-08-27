a39365d780c5b2cdb0faea89d35d353a43b41193
Bruno Degrémont <galibot59@gmail.com>
2021-08-28 00:17:25 +0200

    Add divide method

--- ProGitForProgrammers/Caclulator.cs
+++ ProGitForProgrammers/Caclulator.cs
@@ -10,9 +10,12 @@ namespace ProGitForProgrammers
         public int Substract(int left,int right ){
             return left - right ;
         }
-
         public int Multiply(int left,int right){
             return left * right;
+        }
+
+        public int Divide(int left,int right){
+            return left / right ;
         }
     }
 }
