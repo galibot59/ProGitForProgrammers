f92b3b54d78ddccc20641fce053aacc44c7ad03b
Bruno Degrémont <galibot59@gmail.com>
2021-08-28 00:08:16 +0200

    Create Movie.cs
    Create class Movie and add attribut Title et Actors

---
+++ ProGitForProgrammers/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProGitForProgrammers
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public List<string> Actors { get; set; }
+    }
+}
